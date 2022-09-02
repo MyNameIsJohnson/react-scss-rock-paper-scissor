6269a55c0810b6ddaf770aa44075ab284e0ff0b9
johnson <johnsonle2929@gmail.com>
2022-09-02 14:51:33 -0700

    Fixed color on anchor tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,19 @@ const Footer = () => {
       <footer className="footer">
         <div className="attribution">
           Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            rel="noreferrer"
+            target="_blank"
+          >
             Frontend Mentor
           </a>
           . Coded by{" "}
-          <a href="https://mynameisjohnson.github.io/react-portfolio/">
+          <a
+            href="https://mynameisjohnson.github.io/react-portfolio/"
+            rel="noreferrer"
+            target="_blank"
+          >
             Johnson Thieu
           </a>
           .
