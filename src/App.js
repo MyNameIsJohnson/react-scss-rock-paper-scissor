65a5538e36bac4e50ed808f551caded812f01635
johnson <johnsonle2929@gmail.com>
2022-09-02 14:42:21 -0700

    added comments

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import Game from "./components/Game";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 function App() {
+  // set state for user choice and score state of 0
   const [myChoice, setMyChoice] = useState("");
   const [score, setScore] = useState(0);
   return (
     <>
       <div className="container">
+        {/* Create Header attribute of score equal to state score */}
         <Header score={score} />
         <Routes>
           <Route
             exact
             path="/"
+            //Set setMyChoice attribute on Play component
             element={<Play setMyChoice={setMyChoice} />}
           ></Route>
           <Route
             path="/game"
             element={
+              //Set Game component attributes: myChoice, score, setScore
               <Game myChoice={myChoice} score={score} setScore={setScore} />
             }
           ></Route>
