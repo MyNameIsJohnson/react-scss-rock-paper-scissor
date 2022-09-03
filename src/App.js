d2da47e98940be445e6ca6384bdea126662a1cf9
johnson <johnsonle2929@gmail.com>
2022-09-02 18:26:01 -0700

    Added conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Play from "./components/Play";
 import Game from "./components/Game";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Footer from "./components/Footer";
 function App() {
   // set state for user choice and score state of 0
   const [myChoice, setMyChoice] = useState("");
   const [score, setScore] = useState(0);
+  console.log(myChoice === "");
   return (
     <>
       <div className="container">
@@ -20,13 +21,18 @@ function App() {
             //Set setMyChoice attribute on Play component
             element={<Play setMyChoice={setMyChoice} />}
           ></Route>
-          <Route
-            path="/game"
-            element={
-              //Set Game component attributes: myChoice, score, setScore
-              <Game myChoice={myChoice} score={score} setScore={setScore} />
-            }
-          ></Route>
+          {!myChoice ? (
+            // set all path to home when myChoice is empty string
+            <Route path="*" element={<Navigate to="/" replace />}></Route>
+          ) : (
+            <Route
+              path="/game"
+              element={
+                //Set Game component attributes: myChoice, score, setScore
+                <Game myChoice={myChoice} score={score} setScore={setScore} />
+              }
+            ></Route>
+          )}
         </Routes>
       </div>
       <Footer />
